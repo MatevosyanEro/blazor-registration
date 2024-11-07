7f5761327314a97e3bf87baa54eaade8a95effb6
MatevosyanEro <matevosyanero.02@mail.ru>
2024-11-08 01:59:48 +0400

    signin component created

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -4,6 +4,8 @@ namespace Reg.Services
 	public interface IUserService
 	{
 		Task<int> AddUserAsync(User user);
+		Task<bool> ValidateUserAsync(string email,string password);
+		Task<bool> IsEmailExistingAsync(string email);
 		//Task<User> GetUserByIdAsync(int Id);
 		//Task<List<User>> GetUsersAsync();
 	}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -4,7 +4,7 @@ using Reg.Models;
 using System.Net;
 namespace Reg.Services
 {
-	public class UserService:IUserService
+	public class UserService : IUserService
 	{
 		private readonly AppDbContext _db;
 		public UserService(AppDbContext db)
@@ -27,5 +27,33 @@ namespace Reg.Services
 
 		}
 
+		public async Task<bool> ValidateUserAsync(string email, string password)
+		{
+			try
+			{
+				var user = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+				if (user != null && user.Password == password)
+				{
+					return true;
+				}
+				return false;
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+		
+		public async Task<bool> IsEmailExistingAsync(string email)
+		{
+			var existing_email = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+			if (existing_email != null) {
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }
